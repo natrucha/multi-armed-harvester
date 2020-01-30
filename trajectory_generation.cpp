becf5f6846400c66389876c7ae2b96baa2a24ec3
Natalie Pueyo <npueyos@ucdavis.edu>
2020-01-30 07:31:13 +0000

    began adding jerk limited profile generation to header and cpp trapezoid file so changed name of the "library"

--- trajectory_generation.cpp
+++ trajectory_generation.cpp
@@ -1,37 +1,66 @@
 #include <Arduino.h>
-#include "trapezoid.h"
+#include "jerkConsTraj.h"
 
 const float max_vel  = 1.;
 const float max_acc  = 16.;
 const float max_deac = 16.;
+const float max_jerk = 250.;
+
+float a0f;
 
 // see algorithm in https://sam.ensam.eu/bitstream/handle/10985/11974/LSIS_CEP_2017_BEAREE.pdf?sequence=1&isAllowed=y
-trapezoid xAxis(max_vel, max_acc, max_deac);
+jerkConsTraj zAxis(max_vel, max_acc, max_deac, max_jerk);
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  // xAxis.noJerkProfile(5, 7, 1, &trapVariables[0]);
-  xAxis.noJerkProfile(0, 0.1, 0.5);
+  zAxis.noJerkProfile(0, 0.1, 0.5);
 
-  // Serial.print("reach velocity: ");
-  // Serial.print(xAxis.vr);
-  // Serial.print(" reach acceleration: ");
-  // Serial.print(xAxis.ar);
-  // Serial.print(" reach deacceleration: ");
-  // Serial.println(xAxis.dr);
+  // Serial.print("Total trapezoidal travel time: ");
+  // Serial.println(xAxis.Tt);
+  // Serial.println();
+
+  // Serial.print("ar: ");
+  // Serial.print(zAxis.ar, 4);
+  // Serial.print(" vr: ");
+  // Serial.print(zAxis.vr, 4);
+  // Serial.print(" dr: ");
+  // Serial.print(zAxis.dr, 4);
+  // Serial.println();
   //
-  // Serial.print("Tv: ");
-  // Serial.print(xAxis.Tv);
-  // Serial.print(" Ta: ");
-  // Serial.print(xAxis.Ta);
+  // Serial.print("Ta: ");
+  // Serial.print(zAxis.Ta, 4);
+  // Serial.print(" Tv: ");
+  // Serial.print(zAxis.Tv, 4);
   // Serial.print(" Td: ");
-  // Serial.println(xAxis.Td);
+  // Serial.print(zAxis.Td, 4);
+  // Serial.println();
   // Serial.println();
 
   // xAxis.buildArray();
+  // will need to figure out how to calculate a0f after profile works
+  a0f = 0;
+  zAxis.jerkProfile(a0f); // not very nice is it?
 
-  xAxis.~trapezoid();
+  Serial.print("Tja: ");
+  Serial.print(zAxis.Tja, 4);
+  Serial.print(" Tjv: ");
+  Serial.print(zAxis.Tjv, 4);
+  Serial.print(" Tjd: ");
+  Serial.print(zAxis.Tjd, 4);
+  Serial.println();
+
+  Serial.print("ar: ");
+  Serial.print(zAxis.ar, 4);
+  Serial.print(" vr: ");
+  Serial.print(zAxis.vr, 4);
+  Serial.print(" dr: ");
+  Serial.print(zAxis.dr, 4);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  zAxis.~jerkConsTraj();
 }
