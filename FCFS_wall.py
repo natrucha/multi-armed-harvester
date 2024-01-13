ae2050014fff685a6eda387f6a1ac334da171c5f
Natalie Pueyo <63b8be13d3aeefa40544c7bf@bots.bitbucket.org>
2024-01-12 17:53:08 -0800

    0th fruit was not being harvested because j=0, i=0 so max(Ty,Tz)=inf. Added new calc for Ty, Tz based on vehicle and row locations at t=0

--- FCFS_wall.py
+++ FCFS_wall.py
@@ -1,5 +1,8 @@
 # import math
 import numpy as np
+import sys
+
+from trajectory import *           # import module to calculate the trapezoidal/S-curve (S-curve not working yet) tajectory calculator
 
 class FCFS(object):
     def __init__(self):
@@ -33,13 +36,13 @@ class FCFS(object):
         Z  = [i.z_coord for i in fruit]  # list of fruits' z-coordinate
         TX = [i.Tx for i in fruit]       # list of fruit extension times
 
-        busy_till       = np.zeros([n_row, n_col])    # until when is this arm busy
-        busy_with       = np.zeros([n_row, n_col], dtype=np.int)    # previous chosen fruit for the arm
+        busy_till       = np.zeros([n_row, n_col])                  # when does this arm (c,r) stop being busy and is available for a new job
+        busy_with       = np.zeros([n_row, n_col], dtype=np.int)    # previous chosen fruit to be harvested for the arm (c,r)
 
         # lists of the desired results
         fruit_picked_by = list()                      # list that saves which arm picks which fruit
         fruit_picked_at = list()                      # list that saves at what time an arm picks a fruit, based on fruit_picked_by "topography"
-        self.curr_j          = np.zeros([n_row, n_col])    # array to save the sum of fruits picked by each arm
+        self.curr_j     = np.zeros([n_row, n_col])    # array to save the sum of fruits picked by each arm
         # build out the lists to be the right size (tried replication *, but the lists point at each other)
         for r in range(n_row):
             if n_row  > 1:
@@ -58,7 +61,8 @@ class FCFS(object):
         for i_fruit in N:
             # print('\n\nfruit index being scheduled %d' %i_fruit)
     
-            # calculate the start and end times of the time window during which the fruit is available to be harvested
+            # calculate the start and end times of the time window during which the fruit is available to be harvested (column 0 to column n_col-1)
+            # although the last column is indexed at n_col-1, arange does not include the last value so we want to calulate the last column as n_col (which will not be included)
             tw_s0 = (Y[i_fruit] - (Q + (0 + 1)*d_cell + 0*d_o)) / V_m
             tw_e0 = (Y[i_fruit] - (Q + (0)*d_cell + 0*d_o)) / V_m
             tw_s_last = (Y[i_fruit] - (Q + (n_col + 1)*d_cell + n_col*d_o)) / V_m  # it should be (n_col-1) but using n_col to add another step to tw_s and tw_e because arange doesn't include the stop value 
@@ -72,7 +76,7 @@ class FCFS(object):
             # print('number of columns %d' %n_col)
             # print('start times:', tw_s)
             # print('end times:', tw_e)
-
+ 
             for c_col in range(n_col-1, -1, -1): # needs to include index 0
                 # go backwards because the frontmost arm is most likely to hit fruits first or not be able to harvest them
                 # if tw_s[c_col] <= 0 or tw_e[c_col] <= 0:
@@ -88,9 +92,49 @@ class FCFS(object):
 
                         j_fruit = busy_with[r_row, c_col] # previously harvested fruit
                         # print('previously scheduled fruit index: %d' %j_fruit)
-                        
+
+                        if i_fruit > 0:
+                            j_to_i_travel = fruit_travel_matrix[j_fruit+offset_fruit_index, i_fruit+offset_fruit_index]
+
+                        elif i_fruit == 0:
+                            # if the fruit index is zero, there is no previous fruit to compare against so I should calculate the movement distance from Q and the middle of the row
+                            
+                            ###### IF CHANGING v_max, etc., CHANGE IT IN fruit_handler.py AS WELL!! ########
+
+                            v_max_y   = 1.4 # m/s from Motor Sizing Google sheet calculations if we want to keep triangular profile to maximize speed
+                            a_max_y   = 2.8 # m/s^2 from Motor Sizing Google sheet calculations if we want desired linear movement time to be 1 second
+                            d_max_y   = a_max_y # if motors allow, keep equal to a_max
+                            # initialize the ability to calculate trajectory
+                            traj_calc_y = Trajectory(v_max_y, a_max_y, d_max_y) 
+
+                            # allow calculations for the z-axis, 0.61? m worst case
+                            v_max_z   = 1.3 # m/s from Motor Sizing Google sheet calculations if we want to keep triangular profile to maximize speed
+                            a_max_z   = 2.8 # m/s^2 from Motor Sizing Google sheet calculations if we want desired linear movement time to be 1 second
+                            d_max_z   = a_max_z # if motors allow, keep equal to a_max
+                            # initialize the ability to calculate trajectory
+                            traj_calc_z = Trajectory(v_max_z, a_max_z, d_max_z) 
+
+                            start_y = Q  # in m, vehicle start location
+                            start_z = (z_row_bot_edges[r_row, c_col] + z_row_top_edges[r_row, c_col]) / 2 # in m, centered between the bot and top edge of this row
+
+                            end_y   = sortedFruit[1,0]  # in m, fruit 0's y_coordinate
+                            end_z   = sortedFruit[2,0]  # in m, fruit 0's z_coordinate
+
+                            # calculate side-to-side movement to fruit in y-axis
+                            traj_calc_y.adjInit(start_y, 0.) # start moving from zero speed
+                            traj_calc_y.noJerkProfile(traj_calc_y.q0, end_y, traj_calc_y.v0, v_max_y, a_max_y, d_max_y)
+                            T_y = traj_calc_y.Ta + traj_calc_y.Tv + traj_calc_y.Td 
+                            
+                            # calculate up and down movement to fruit in z-axis
+                            traj_calc_z.adjInit(start_z, 0.) # start moving from zero speed
+                            traj_calc_z.noJerkProfile(traj_calc_z.q0, end_z, traj_calc_z.v0, v_max_z, a_max_z, d_max_z)
+                            T_z = traj_calc_z.Ta + traj_calc_z.Tv + traj_calc_z.Td 
+
+                            # add to matrix and mirror it
+                            j_to_i_travel = max(T_y, T_z)
+                       
                         # check if the arm is busy up to tw_e of the current fruit plus the amount of time it would take to move to the fruit, extend, and grab it
-                        if busy_till[r_row, c_col] + fruit_travel_matrix[j_fruit+offset_fruit_index, i_fruit+offset_fruit_index] + TX[i_fruit] + t_grab <= tw_e[c_col]:
+                        if busy_till[r_row, c_col] + j_to_i_travel + TX[i_fruit] + t_grab <= tw_e[c_col]:
                             # mark the fruit as picked, save by which arm, and increase that arm's count of harvested fruits
                             if n_row > 1:
                                 fruit_picked_by[r_row][c_col].append(i_fruit+offset_fruit_index)
@@ -102,16 +146,16 @@ class FCFS(object):
                             sortedFruit[4, i_fruit+offset_fruit_index] = 1  # save to the real index on sortedFruit
 
                             # if busy_till[r_row, c_col] <= tw_s[c_col]:
-                            if busy_till[r_row, c_col] <= tw_s[c_col] - (fruit_travel_matrix[j_fruit+offset_fruit_index, i_fruit+offset_fruit_index] + TX[i_fruit]): 
+                            if busy_till[r_row, c_col] <= tw_s[c_col] - (j_to_i_travel + TX[i_fruit]): 
                                 # will have to wait until it harvests the next fruit, but as FCFS it would still be chosen as the fruit it will harvest next
                                 # first steps for harvesting can be started before the fruit even reaches the work volume as long as the arm is waiting sp that 
                                 # the time of harvest, t_harvest, is at tw_s + grabbing
                                 t_harvest = tw_s[c_col] + t_grab
-                                # t_harvest = tw_s[c_col] + (fruit_travel_matrix[j_fruit+offset_fruit_index, i_fruit+offset_fruit_index] + TX[i_fruit] + t_grab)
+                                # t_harvest = tw_s[c_col] + (j_to_i_travel + TX[i_fruit] + t_grab)
 
                             else:
                                 # does not wait between one fruit and the other, will start whenever the arm finishes harvesting the previous fruit
-                                t_harvest = busy_till[r_row, c_col] + fruit_travel_matrix[j_fruit+offset_fruit_index, i_fruit+offset_fruit_index] + TX[i_fruit] + t_grab
+                                t_harvest = busy_till[r_row, c_col] + j_to_i_travel + TX[i_fruit] + t_grab
 
                             # print('\narm in col %d and row %d is busy until %0.2f with fruit %d and the next harvest time is %0.2f for fruit %d' %(c_col, r_row, busy_till[r_row, c_col], busy_with[r_row, c_col], t_harvest, i_fruit))
                                 
@@ -148,7 +192,7 @@ class FCFS(object):
         # print('\nThe list of when an arm picks the next fruit:')
         # print(fruit_picked_at)
         # print('and the array showing how many fruit were picked by each arm:')  
-        # print(self.curr_j)                              
+        # print(self.curr_j)   
 
         # init cannot return a list, currently being used as sys.exit(0)
         return([fruit_picked_by, fruit_picked_at])
