ef7f7cbeffe1a0a6d91c6c73b85d7cbf0940874a
Natalie Pueyo <63b8be13d3aeefa40544c7bf@bots.bitbucket.org>
2024-06-10 02:48:48 -0700

    added licensing information

--- IG_data_analysis.py
+++ IG_data_analysis.py
@@ -1,3 +1,20 @@
+# Data analysis functions for the multi-armed harvester
+# Copyright (C) 2024  Natalie C. Pueyo Svoboda
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+# 
+
 ## based on https://dynetworkx.readthedocs.io/en/latest/tutorial.html
 # import dynetworkx as dnx
 # import networkx as nx
--- fruit_distribution.py
+++ fruit_distribution.py
@@ -1,4 +1,21 @@
-import csv                      # read and write CSV file-type
+# Creates or reads the desired fruit distributions
+# Copyright (C) 2024  Natalie C. Pueyo Svoboda
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+# 
+# 
+# import csv                      # read and write CSV file-type
 import numpy as np
 # from numpy.random import PCG64
 
