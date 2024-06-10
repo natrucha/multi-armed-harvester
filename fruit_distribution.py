492f33c0b93a74d28cabcf1cb5628fcba83e9b7c
Natalie Pueyo <63b8be13d3aeefa40544c7bf@bots.bitbucket.org>
2024-06-10 13:42:22 -0700

    minor cleanup

--- fruit_distribution.py
+++ fruit_distribution.py
@@ -14,8 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>. 
 # 
-# 
-# import csv                      # read and write CSV file-type
+
+import csv                      # read and write CSV file-type
 import numpy as np
 # from numpy.random import PCG64
 
